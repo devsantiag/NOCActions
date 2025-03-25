6f447495dcf2646311bd3bc6af476be9d6da5ced
santiago <98044979+devsantiag@users.noreply.github.com>
2025-03-25 10:21:56 -0300

    Added reference to Microsoft.Win32 library for access to Windows Registry functionality

---
+++ CONFIG_ComunicacaoComCliente.Designer.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NOCActions
+{
+    public partial class CONFIG_ComunicacaoComCliente : Form
+    {
+        private SplitContainer splitContainer1;
+        private Label labelCliente, labelEmail, labelUnidade, labelEndereco, labelInfo;
+        private Label labelID;
+        private ColumnHeader columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1UserID;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.ListView listView1ListaDeClientesJaAdicionados;
+        private System.Windows.Forms.TextBox comboNomeCliente;
+        private System.Windows.Forms.TextBox comboEnderecoCliente;
+        private System.Windows.Forms.TextBox comboUnidadeDoCliente;
+        private System.Windows.Forms.TextBox comboRazaoSocialCliente;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox comboEmailContatoCliente;
+        private System.Windows.Forms.Label combo;
+
+//        public CONFIG_ComunicacaoComCliente()
+//        {
+//            InitializeComponent();
+//        }
+
+        private void InitializeComponent()
+        {
+        	System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+        	System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+        	System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("");
+        	System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("");
+        	System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("");
+        	System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("");
+        	System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem("");
+        	System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem("");
+        	System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem("");
+        	this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+        	this.labelEmail = new System.Windows.Forms.Label();
+        	this.labelUnidade = new System.Windows.Forms.Label();
+        	this.labelEndereco = new System.Windows.Forms.Label();
+        	this.labelInfo = new System.Windows.Forms.Label();
+        	this.labelCliente = new System.Windows.Forms.Label();
+        	this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+        	this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+        	this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+        	this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+        	this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+        	this.listView1ListaDeClientesJaAdicionados = new System.Windows.Forms.ListView();
+        	this.columnHeader8 = new System.Windows.Forms.ColumnHeader();
+        	this.columnHeader9 = new System.Windows.Forms.ColumnHeader();
+        	this.label1 = new System.Windows.Forms.Label();
+        	this.label2 = new System.Windows.Forms.Label();
+        	this.btnSair = new System.Windows.Forms.Button();
+        	this.btnSalvar = new System.Windows.Forms.Button();
+        	this.btnExcluir = new System.Windows.Forms.Button();
+        	this.comboNomeCliente = new System.Windows.Forms.TextBox();
+        	this.comboEnderecoCliente = new System.Windows.Forms.TextBox();
+        	this.comboUnidadeDoCliente = new System.Windows.Forms.TextBox();
+        	this.comboRazaoSocialCliente = new System.Windows.Forms.TextBox();
+        	this.label3 = new System.Windows.Forms.Label();
+        	this.label4 = new System.Windows.Forms.Label();
+        	this.labelID = new System.Windows.Forms.Label();
+        	this.maskedTextBox1UserID = new System.Windows.Forms.MaskedTextBox();
+        	this.button1 = new System.Windows.Forms.Button();
+        	this.comboEmailContatoCliente = new System.Windows.Forms.TextBox();
+        	this.combo = new System.Windows.Forms.Label();
+        	((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+        	this.splitContainer1.SuspendLayout();
+        	this.SuspendLayout();
+        	// 
+        	// splitContainer1
+        	// 
+        	this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+        	this.splitContainer1.Name = "splitContainer1";
+        	this.splitContainer1.Size = new System.Drawing.Size(150, 100);
+        	this.splitContainer1.TabIndex = 0;
+        	// 
+        	// labelEmail
+        	// 
+        	this.labelEmail.AutoSize = true;
+        	this.labelEmail.Font = new System.Drawing.Font("Segoe UI", 12F);
+        	this.labelEmail.Location = new System.Drawing.Point(30, 132);
+        	this.labelEmail.Name = "labelEmail";
+        	this.labelEmail.Size = new System.Drawing.Size(97, 21);
+        	this.labelEmail.TabIndex = 3;
+        	this.labelEmail.Text = "Razão Social";
+        	// 
+        	// labelUnidade
+        	// 
+        	this.labelUnidade.AutoSize = true;
+        	this.labelUnidade.Font = new System.Drawing.Font("Segoe UI", 12F);
+        	this.labelUnidade.Location = new System.Drawing.Point(59, 97);
+        	this.labelUnidade.Name = "labelUnidade";
+        	this.labelUnidade.Size = new System.Drawing.Size(68, 21);
+        	this.labelUnidade.TabIndex = 4;
+        	this.labelUnidade.Text = "Unidade";
+        	// 
+        	// labelEndereco
+        	// 
+        	this.labelEndereco.AutoSize = true;
+        	this.labelEndereco.Font = new System.Drawing.Font("Segoe UI", 12F);
+        	this.labelEndereco.Location = new System.Drawing.Point(53, 62);
+        	this.labelEndereco.Name = "labelEndereco";
+        	this.labelEndereco.Size = new System.Drawing.Size(74, 21);
+        	this.labelEndereco.TabIndex = 5;
+        	this.labelEndereco.Text = "Endereço";
+        	// 
+        	// labelInfo
+        	// 
+        	this.labelInfo.AutoSize = true;
+        	this.labelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.labelInfo.ForeColor = System.Drawing.Color.Red;
+        	this.labelInfo.Location = new System.Drawing.Point(59, 544);
+        	this.labelInfo.Name = "labelInfo";
+        	this.labelInfo.Size = new System.Drawing.Size(594, 13);
+        	this.labelInfo.TabIndex = 1;
+        	this.labelInfo.Text = "ADICIONAR NFORMAÇÕES DO CLIENTE E CONTRATO PARA CONTATAR VIA MENSAGEM DE EMAIL";
+        	// 
+        	// labelCliente
+        	// 
+        	this.labelCliente.AutoSize = true;
+        	this.labelCliente.Font = new System.Drawing.Font("Segoe UI", 12F);
+        	this.labelCliente.Location = new System.Drawing.Point(257, 26);
+        	this.labelCliente.Name = "labelCliente";
+        	this.labelCliente.Size = new System.Drawing.Size(58, 21);
+        	this.labelCliente.TabIndex = 2;
+        	this.labelCliente.Text = "Cliente";
+        	// 
+        	// listView1ListaDeClientesJaAdicionados
+        	// 
+        	this.listView1ListaDeClientesJaAdicionados.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+			this.columnHeader8,
+			this.columnHeader9});
+        	this.listView1ListaDeClientesJaAdicionados.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.listView1ListaDeClientesJaAdicionados.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+			listViewItem1,
+			listViewItem2,
+			listViewItem3,
+			listViewItem4,
+			listViewItem5,
+			listViewItem6,
+			listViewItem7,
+			listViewItem8,
+			listViewItem9});
+        	this.listView1ListaDeClientesJaAdicionados.Location = new System.Drawing.Point(53, 235);
+        	this.listView1ListaDeClientesJaAdicionados.Name = "listView1ListaDeClientesJaAdicionados";
+        	this.listView1ListaDeClientesJaAdicionados.Size = new System.Drawing.Size(600, 183);
+        	this.listView1ListaDeClientesJaAdicionados.TabIndex = 13;
+        	this.listView1ListaDeClientesJaAdicionados.UseCompatibleStateImageBehavior = false;
+        	this.listView1ListaDeClientesJaAdicionados.View = System.Windows.Forms.View.Details;
+        	// 
+        	// columnHeader8
+        	// 
+        	this.columnHeader8.Text = "Cliente";
+        	// 
+        	// columnHeader9
+        	// 
+        	this.columnHeader9.Text = "Unidade";
+        	// 
+        	// label1
+        	// 
+        	this.label1.AutoSize = true;
+        	this.label1.ForeColor = System.Drawing.SystemColors.ScrollBar;
+        	this.label1.Location = new System.Drawing.Point(112, 212);
+        	this.label1.Name = "label1";
+        	this.label1.Size = new System.Drawing.Size(541, 13);
+        	this.label1.TabIndex = 14;
+        	this.label1.Text = "_________________________________________________________________________________" +
+	"________";
+        	// 
+        	// label2
+        	// 
+        	this.label2.AutoSize = true;
+        	this.label2.Font = new System.Drawing.Font("Segoe UI", 12F);
+        	this.label2.Location = new System.Drawing.Point(53, 211);
+        	this.label2.Name = "label2";
+        	this.label2.Size = new System.Drawing.Size(65, 21);
+        	this.label2.TabIndex = 15;
+        	this.label2.Text = "Clientes";
+        	// 
+        	// btnSair
+        	// 
+        	this.btnSair.Font = new System.Drawing.Font("Segoe UI", 12F);
+        	this.btnSair.Location = new System.Drawing.Point(551, 425);
+        	this.btnSair.Name = "btnSair";
+        	this.btnSair.Size = new System.Drawing.Size(102, 48);
+        	this.btnSair.TabIndex = 16;
+        	this.btnSair.Text = "Sair";
+        	this.btnSair.UseVisualStyleBackColor = true;
+        	// 
+        	// btnSalvar
+        	// 
+        	this.btnSalvar.Font = new System.Drawing.Font("Segoe UI", 12F);
+        	this.btnSalvar.Location = new System.Drawing.Point(269, 425);
+        	this.btnSalvar.Name = "btnSalvar";
+        	this.btnSalvar.Size = new System.Drawing.Size(124, 51);
+        	this.btnSalvar.TabIndex = 17;
+        	this.btnSalvar.Text = "Salvar";
+        	this.btnSalvar.UseVisualStyleBackColor = true;
+        	this.btnSalvar.Click += new System.EventHandler(this.BtnSalvarClick);
+        	// 
+        	// btnExcluir
+        	// 
+        	this.btnExcluir.Font = new System.Drawing.Font("Segoe UI", 12F);
+        	this.btnExcluir.Location = new System.Drawing.Point(399, 425);
+        	this.btnExcluir.Name = "btnExcluir";
+        	this.btnExcluir.Size = new System.Drawing.Size(124, 51);
+        	this.btnExcluir.TabIndex = 18;
+        	this.btnExcluir.Text = "Excluir";
+        	this.btnExcluir.UseVisualStyleBackColor = true;
+        	// 
+        	// comboNomeCliente
+        	// 
+        	this.comboNomeCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.comboNomeCliente.Location = new System.Drawing.Point(336, 26);
+        	this.comboNomeCliente.Name = "comboNomeCliente";
+        	this.comboNomeCliente.Size = new System.Drawing.Size(317, 24);
+        	this.comboNomeCliente.TabIndex = 10000;
+        	// 
+        	// comboEnderecoCliente
+        	// 
+        	this.comboEnderecoCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.comboEnderecoCliente.Location = new System.Drawing.Point(141, 61);
+        	this.comboEnderecoCliente.Name = "comboEnderecoCliente";
+        	this.comboEnderecoCliente.Size = new System.Drawing.Size(512, 24);
+        	this.comboEnderecoCliente.TabIndex = 10001;
+        	// 
+        	// comboUnidadeDoCliente
+        	// 
+        	this.comboUnidadeDoCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.comboUnidadeDoCliente.Location = new System.Drawing.Point(141, 96);
+        	this.comboUnidadeDoCliente.Name = "comboUnidadeDoCliente";
+        	this.comboUnidadeDoCliente.Size = new System.Drawing.Size(512, 24);
+        	this.comboUnidadeDoCliente.TabIndex = 10002;
+        	// 
+        	// comboRazaoSocialCliente
+        	// 
+        	this.comboRazaoSocialCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.comboRazaoSocialCliente.Location = new System.Drawing.Point(141, 131);
+        	this.comboRazaoSocialCliente.Name = "comboRazaoSocialCliente";
+        	this.comboRazaoSocialCliente.Size = new System.Drawing.Size(512, 24);
+        	this.comboRazaoSocialCliente.TabIndex = 10003;
+        	// 
+        	// label3
+        	// 
+        	this.label3.AutoSize = true;
+        	this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.label3.ForeColor = System.Drawing.Color.Red;
+        	this.label3.Location = new System.Drawing.Point(53, 499);
+        	this.label3.Name = "label3";
+        	this.label3.Size = new System.Drawing.Size(21, 13);
+        	this.label3.TabIndex = 10004;
+        	this.label3.Text = "01";
+        	// 
+        	// label4
+        	// 
+        	this.label4.AutoSize = true;
+        	this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.label4.ForeColor = System.Drawing.Color.Red;
+        	this.label4.Location = new System.Drawing.Point(346, 531);
+        	this.label4.Name = "label4";
+        	this.label4.Size = new System.Drawing.Size(277, 13);
+        	this.label4.TabIndex = 10005;
+        	this.label4.Text = "feat: sistema de cadastro e listagem de clientes";
+        	// 
+        	// labelID
+        	// 
+        	this.labelID.AutoSize = true;
+        	this.labelID.Font = new System.Drawing.Font("Segoe UI", 12F);
+        	this.labelID.Location = new System.Drawing.Point(53, 24);
+        	this.labelID.Name = "labelID";
+        	this.labelID.Size = new System.Drawing.Size(25, 21);
+        	this.labelID.TabIndex = 10;
+        	this.labelID.Text = "ID";
+        	// 
+        	// maskedTextBox1UserID
+        	// 
+        	this.maskedTextBox1UserID.Location = new System.Drawing.Point(141, 26);
+        	this.maskedTextBox1UserID.Name = "maskedTextBox1UserID";
+        	this.maskedTextBox1UserID.ReadOnly = true;
+        	this.maskedTextBox1UserID.Size = new System.Drawing.Size(56, 20);
+        	this.maskedTextBox1UserID.TabIndex = 9999;
+        	// 
+        	// button1
+        	// 
+        	this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+        	this.button1.Location = new System.Drawing.Point(139, 425);
+        	this.button1.Name = "button1";
+        	this.button1.Size = new System.Drawing.Size(124, 51);
+        	this.button1.TabIndex = 10008;
+        	this.button1.Text = "Modelo E-mail";
+        	this.button1.UseVisualStyleBackColor = true;
+        	// 
+        	// comboEmailContatoCliente
+        	// 
+        	this.comboEmailContatoCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+        	this.comboEmailContatoCliente.Location = new System.Drawing.Point(141, 166);
+        	this.comboEmailContatoCliente.Name = "comboEmailContatoCliente";
+        	this.comboEmailContatoCliente.Size = new System.Drawing.Size(512, 24);
+        	this.comboEmailContatoCliente.TabIndex = 10010;
+        	// 
+        	// combo
+        	// 
+        	this.combo.AutoSize = true;
+        	this.combo.Font = new System.Drawing.Font("Segoe UI", 12F);
+        	this.combo.Location = new System.Drawing.Point(73, 167);
+        	this.combo.Name = "combo";
+        	this.combo.Size = new System.Drawing.Size(54, 21);
+        	this.combo.TabIndex = 10009;
+        	this.combo.Text = "E-mail";
+        	// 
+        	// CONFIG_ComunicacaoComCliente
+        	// 
+        	this.ClientSize = new System.Drawing.Size(701, 566);
+        	this.Controls.Add(this.comboEmailContatoCliente);
+        	this.Controls.Add(this.combo);
+        	this.Controls.Add(this.button1);
+        	this.Controls.Add(this.label4);
+        	this.Controls.Add(this.label3);
+        	this.Controls.Add(this.comboRazaoSocialCliente);
+        	this.Controls.Add(this.comboUnidadeDoCliente);
+        	this.Controls.Add(this.comboEnderecoCliente);
+        	this.Controls.Add(this.comboNomeCliente);
+        	this.Controls.Add(this.maskedTextBox1UserID);
+        	this.Controls.Add(this.btnExcluir);
+        	this.Controls.Add(this.btnSalvar);
+        	this.Controls.Add(this.btnSair);
+        	this.Controls.Add(this.label1);
+        	this.Controls.Add(this.label2);
+        	this.Controls.Add(this.listView1ListaDeClientesJaAdicionados);
+        	this.Controls.Add(this.labelID);
+        	this.Controls.Add(this.labelInfo);
+        	this.Controls.Add(this.labelCliente);
+        	this.Controls.Add(this.labelEmail);
+        	this.Controls.Add(this.labelUnidade);
+        	this.Controls.Add(this.labelEndereco);
+        	this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+        	this.MaximizeBox = false;
+        	this.Name = "CONFIG_ComunicacaoComCliente";
+        	this.Text = "Adicionar Novo Cliente";
+        	((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+        	this.splitContainer1.ResumeLayout(false);
+        	this.ResumeLayout(false);
+        	this.PerformLayout();
+
+        }
+    }
+}
---
+++ CONFIG_ComunicacaoComCliente.cs
@@ -0,0 +1,129 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: fjstavares
+ * Date: 26/02/2025
+ * Time: 15:54
+ */
+
+using System;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace NOCActions
+{
+	public partial class CONFIG_ComunicacaoComCliente : Form
+	{
+		private Timer timer;
+		
+		public CONFIG_ComunicacaoComCliente()
+		{
+			InitializeComponent();
+			OrdenarTabIndex();
+			TimerGeneratorReload();
+			CarregarInformacoesDeRegistro(); // Carrega os dados ao iniciar
+		}
+		
+		public CONFIG_ComunicacaoComCliente(string V1, string V2, string V3, string V4, string V5)
+		{
+			InitializeComponent();
+			
+			comboNomeCliente.Text = V1;
+			comboEnderecoCliente.Text = V2;
+			comboUnidadeDoCliente.Text = V3;
+			comboRazaoSocialCliente.Text = V4;
+			comboEmailContatoCliente.Text = V5;
+		}
+
+//		Button responsável por salvar as informações do contrato
+		private void SalvarInformacoesDoFormulario()
+		{
+			try
+			{
+				RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\NOCActions\Cliente");
+				
+				key.SetValue("NomeCliente", comboNomeCliente.Text);
+				key.SetValue("EnderecoCliente", comboEnderecoCliente.Text);
+				key.SetValue("UnidadeCliente", comboUnidadeDoCliente.Text);
+				key.SetValue("RazaoSocialCliente", comboRazaoSocialCliente.Text);
+				key.SetValue("EmailContatoCliente", comboEmailContatoCliente.Text);
+				
+				key.Close();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Erro ao salvar os dados: " + ex.Message);
+			}
+		}
+		
+		private void CarregarInformacoesDeRegistro()
+		{
+			try {
+				RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\NOCActions\Cliente");
+
+				if (key != null)
+				{
+					comboNomeCliente.Text = key.GetValue("NomeCliente", "").ToString();
+					comboEnderecoCliente.Text = key.GetValue("EnderecoCliente", "").ToString();
+					comboUnidadeDoCliente.Text = key.GetValue("UnidadeCliente", "").ToString();
+					comboRazaoSocialCliente.Text = key.GetValue("RazaoSocialCliente", "").ToString();
+					comboEmailContatoCliente.Text = key.GetValue("EmailContatoCliente", "").ToString();
+
+					key.Close();
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Erro ao carregar os dados: " + ex.Message);
+			}
+		}
+			
+//		organiza o TAB
+			private void OrdenarTabIndex()
+			{
+				comboNomeCliente.TabIndex = 0;
+				comboEnderecoCliente.TabIndex = 1;
+				comboUnidadeDoCliente.TabIndex = 2;
+				comboRazaoSocialCliente.TabIndex = 3;
+				btnSalvar.TabIndex = 4;
+				btnExcluir.TabIndex = 5;
+				btnSair.TabIndex = 6;
+			}
+			
+			private void TimerGeneratorReload()
+			{
+				timer = new Timer();
+				timer.Interval = 2000; // Intervalo de 2 segundos
+				timer.Tick += Timer_Tick;
+				timer.Start();
+			}
+			
+			private void GerarIDUnico()
+			{
+				string uniqueID = "T&C" + Guid.NewGuid().ToString("N").Substring(0, 10); // Pega os 10 primeiros caracteres
+				maskedTextBox1UserID.Text = uniqueID;
+			}
+			
+			private void Timer_Tick(object sender, EventArgs e)
+			{
+				GerarIDUnico();
+				timer.Stop(); // Para o timer após a geração do ID
+			}
+			
+//		button responsável por salvar informações do novo contrato
+			void BtnSalvarClick(object sender, EventArgs e)
+			{
+				SalvarInformacoesDoFormulario();
+				
+				string nomeCliente = comboNomeCliente.Text;
+				string enderecoCliente = comboEnderecoCliente.Text;
+				string unidadeCliente = comboUnidadeDoCliente.Text;
+				string razaoSocialCliente = comboRazaoSocialCliente.Text;
+				string emailContatoCliente = comboEmailContatoCliente.Text;
+				
+				ACAO_ComunicacaoComCliente acaoForm = new ACAO_ComunicacaoComCliente(nomeCliente, enderecoCliente, unidadeCliente, razaoSocialCliente, emailContatoCliente);
+				acaoForm.Show();
+				
+				
+			}
+		}
+	}
